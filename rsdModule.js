48ec5b646db21c99f84c725c30d4346755d2a283
Amogh Chakravarthy <amogh@Amoghs-MacBook-Pro.local>
2018-10-20 12:04:56 +0530

    Adding Client Secret to Project for Deployment

--- rsdModule.js
+++ rsdModule.js
@@ -27,7 +27,7 @@ var googleAuth = require('google-auth-library');
 var SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly'];
 var TOKEN_DIR = (process.env.HOME || process.env.HOMEPATH || process.env.USERPROFILE) + '/.credentials/';
 var TOKEN_PATH = TOKEN_DIR + 'sheets.googleapis.com-nodejs-rsdAuth.json';
-var CLIENT_SECRET = 'Rapid Sheet Data Service-be67afaae764.json';   // filename eg.: "rsd_example_secret.json"
+var CLIENT_SECRET = 'ww-rsd-service-24456c400ad7.json';   // filename eg.: "rsd_example_secret.json"
 
 
 // 
